6b2ce8b2e305695947183a229a4bf9ea5889768f
Developer_02 <Developer_02@gmail.com>
2021-02-05 12:31:54 +0530

    Developer 02 : File 02 Updates

--- Java_Git_Repository/src/org/project/Test_02.java
+++ Java_Git_Repository/src/org/project/Test_02.java
@@ -4,7 +4,7 @@ public class Test_02 {
 
 	public static void main(String args[]) {
 		
-		System.out.println("This is another source file");
+		System.out.println("This is another source file. Now Updated by Developer 02");
 		
 	}
 	
