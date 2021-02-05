7881bfb9b36261254d7e3129e0dced5f6e40e5af
Developer_02 <Developer_02@gmail.com>
2021-02-05 16:22:40 +0530

    Update from Developer 02.

--- Java_Git_Repository/src/org/project/Test_02.java
+++ Java_Git_Repository/src/org/project/Test_02.java
@@ -6,11 +6,13 @@ public class Test_02 {
 		
 		System.out.println("This is another source file. Now Updated by Developer 02");
 		
-		System.out.println("New Updates from Developer 01");
+		System.out.println("1.New Updates from Developer 01");
 		
-		System.out.println("New Updates from Developer 02");
+		System.out.println("2.New Updates from Developer 02");
 		
-		System.out.println("New Updates from Developer 01");
+		System.out.println("3.New Updates from Developer 01");
+		
+		System.out.println("4.New Updates from Developer 02");
 	}
 	
 }
